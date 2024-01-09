501269c097c65f18368261a042d3afa4d6a913ca
Carolyn Whelpley <33427006+mcdonaldcarolyn@users.noreply.github.com>
2024-01-09 16:44:50 +0000

    fix typo

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ def askForGuess():
     while True:
         guess = input('> ')
 
-        if guess.isDecimal():
+        if guess.isdecimal():
             return int(guess)
         print('please enter a number between 1 and 100')
 
 print('guess the number by inventwithpython')
 print()
-secretNumber = random.randint(1,100)
+secretNumber = random.randint(1, 100)
 print('I am thinking of a number between 1 and 100')
 
 for i in range(10):
@@ -19,12 +19,13 @@ for i in range(10):
     guess = askForGuess()
     if guess == secretNumber:
         break
+    
     if guess < secretNumber:
-        print('your guess is too late.')
+        print('your guess is too low.')
     if guess > secretNumber:
         print('Your guess is too high.')
 
 if guess == secretNumber:
     print('Yay, you got it!')
 else:
-    print('Game over, the correct number was', seceretNumber)
+    print('Game over, the correct number was', secretNumber)
