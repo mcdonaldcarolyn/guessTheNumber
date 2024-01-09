3c39de30085ef8c49b8d19751512844ce2988f1c
Carolyn Whelpley <33427006+mcdonaldcarolyn@users.noreply.github.com>
2024-01-09 15:25:10 +0000

    generate the random number

--- main.py
+++ main.py
@@ -8,4 +8,6 @@ def askForGuess():
             return int(guess)
         print('please enter a number between 1 and 100')
 
-        
+print('guess the number by inventwithpython')
+print()
+secretNumber = random.randint(1,100)
