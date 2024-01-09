6b9cb46400a0527ca509fe980d8e133ac48228c3
Carolyn Whelpley <33427006+mcdonaldcarolyn@users.noreply.github.com>
2024-01-09 15:21:51 +0000

    create input for guess

--- main.py
+++ main.py
@@ -3,4 +3,9 @@ import random
 def askForGuess():
     while True:
         guess = input('> ')
+
+        if guess.isDecimal():
+            return int(guess)
+        print('please enter a number between 1 and 100')
+
         
